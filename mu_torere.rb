5d6487833745a7cdd226dbf13966af9f751a766f
Rambo <mael.arnaud@gmail.com>
2015-10-04 12:10:28 +0200

    Added the AI moves and refactored the play sequence

--- mu_torere.rb
+++ mu_torere.rb
@@ -1,44 +1,74 @@
 #encoding: UTF-8
+
 require_relative 'board'
+require_relative 'mt_tools'
+
+def recursive_require(file)
+        puts file
+        if File.directory?(file)
+                Dir[file + '/*'].each { |inner_file| recursive_require(inner_file) }
+        else
+                require file
+        end
+end
+
+recursive_require('./AI')
+
 class Mu_Torere
-        attr_accessor :game_board
+        attr_reader :game_board, :lost
 
         def initialize()
                 @game_board = Game_Board.new()
+                @lost = false
                 @current_player = nil
-                next_player()
+                @ai = AI.new(
+                        Hill_Climbing,
+                        Minimize_Ennemy_Plays,
+                        'A'
+                )
         end
 
         def next_player()
                 @current_player = @current_player == "A" ? "B" : "A"
-                lost?()
         end
 
         def move()
-                # This is where the playing takes place
-                puts @game_board.to_s
                 p "Player : " + @current_player
-
-                input = gets.to_i
-
-                while !@game_board.can_be_moved(input, @current_player)
-                        p "You can't move that piece"
+                puts @game_board.to_s
+                if @current_player == @ai.player
+                        @ai.play(@game_board)
+                else
+                        # This is where the playing takes place
                         input = gets.to_i
-                end
 
-                @game_board.move(input)
-                next_player()
+                        while !@game_board.can_be_moved(
+                                        input, 
+                                        @current_player
+                        )
+                                p "You can't move that piece"
+                                input = gets.to_i
+                        end
+                        @game_board.move(input)
+                end
         end
 
         def lost?()
-                @game_board.lost?(@current_player) ?  you_lost() : move()
+                you_lost() if @game_board.lost?(@current_player)
         end
 
         def you_lost()
                 puts @game_board.to_s
                 p "Player : " + @current_player + " lost the game."
+                @lost = true
         end
 end
 
 game = Mu_Torere.new()
-
+game.next_player
+while !game.lost
+        game.move
+        game.next_player
+        game.lost?
+end
+        
+        
