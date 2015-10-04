73ac4e8bc1d9264cc3215d80ce56effdfe1405a5
Rambo <mael.arnaud@gmail.com>
2015-10-04 12:09:49 +0200

    Changes to adapt the board to the AI

--- board.rb
+++ board.rb
@@ -1,16 +1,27 @@
 #encoding: UTF-8
 require_relative 'piece'
 class Game_Board
-        attr_accessor :board, :putahi
+        attr_reader :outer_board, :putahi
 
-        def initialize()
-                @board = Array.new
-                8.times do 
-                        @board << Piece.new(
-                                @board.length > 3 ? "B" : "A"
-                        )
+        def initialize(outer_board = nil, putahi = nil)
+                if !outer_board
+                        @outer_board = Array.new
+                        8.times do 
+                                @outer_board << Piece.new(
+                                        @outer_board.length > 3 ? "B" : "A"
+                                )   
+                        end
+                else
+                        @outer_board = outer_board
                 end
-                @putahi = nil
+                 @putahi = putahi
+        end
+
+        def clone()
+                clone = Game_Board.new(
+                        @outer_board.clone, 
+                        @putahi ? @putahi.clone : nil
+                )
         end
 
         def get_string_value(piece)
@@ -24,28 +35,32 @@ class Game_Board
                         when index == 3
                                 array.last[7] = get_string_value(@putahi)
                         when index%2 == 0
-                                array.last[2] = get_string_value(@board[index/2])
-                                array.last[12] = get_string_value(@board[-1-index/2])
+                                array.last[2] = get_string_value(
+                                        @outer_board[index/2]
+                                )
+                                array.last[12] = get_string_value(
+                                        @outer_board[-1-index/2]
+                                )
                         end
                         array
                 end
         end
 
         def get_piece(spot)
-                spot == 9 ? @putahi : @board[spot-1]
+                spot == 9 ? @putahi : @outer_board[spot-1]
         end
 
         def get_next(spot)
                 # given spot is at index +1, same as length
-                if spot + 1 >= @board.length
-                        @board[0]
+                if spot >= @outer_board.length
+                        @outer_board[0]
                 else
-                        @board[spot]
+                        @outer_board[spot]
                 end
         end
 
         def get_previous(spot)
-                @board[spot-2]
+                @outer_board[spot-2]
         end
 
         def can_be_moved(spot, cp)
@@ -61,7 +76,8 @@ class Game_Board
                         return true
                 when prev == nil || nex == nil 
                         return true
-                when @putahi == nil && (prev.player != cp || nex.player != cp)
+                when @putahi == nil && 
+                     (prev.player != cp || nex.player != cp)
                         return true
                 else
                         return false
@@ -72,26 +88,26 @@ class Game_Board
                 piece = get_piece(spot)
                 case
                 when spot == 9
-                        @board.each_with_index do |p, i|
-                                @board[i] = piece if !p
+                        @outer_board.each_with_index do |p, i|
+                                @outer_board[i] = piece if !p
                         end
                 when !@putahi
                         @putahi = piece
                 when !get_previous(spot)
-                        @board[spot-2] = piece
+                        @outer_board[spot-2] = piece
                 when !get_next(spot)
-                        if spot == @board.length
-                             @board[0] = piece
+                        if spot == @outer_board.length
+                             @outer_board[0] = piece
                         else
-                             @board[spot] = piece
+                             @outer_board[spot] = piece
                         end
                 end
-                spot == 9 ? @putahi = nil : @board[spot-1] = nil
+                spot == 9 ? @putahi = nil : @outer_board[spot-1] = nil
         end
 
         def lost?(cp)
                 return false  if can_be_moved(9, cp)
-                @board.each_with_index do |piece, spot|
+                @outer_board.each_with_index do |piece, spot|
                         if can_be_moved(spot+1, cp) 
                                 return false
                         end
