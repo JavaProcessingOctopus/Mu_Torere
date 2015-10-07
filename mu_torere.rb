cd4735acab6444c2a324db61fd61e6bc170d055b
Rambo <mael.arnaud@gmail.com>
2015-10-07 20:37:48 +0200

    IA created with MinMax algorithm

--- mu_torere.rb
+++ mu_torere.rb
@@ -22,7 +22,7 @@ class Mu_Torere
                 @lost = false
                 @current_player = nil
                 @ai = AI.new(
-                        Hill_Climbing,
+                        MinMax,
                         Minimize_Ennemy_Plays,
                         'A'
                 )
