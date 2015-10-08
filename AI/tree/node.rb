087dce70c5fdd3913bccb8b5c13a1cd16744beb9
JavaProcessingOctopus <frozenthunder@hotmail.com>
2015-10-08 16:22:10 +0200

    Changing logic of nodes 0.2

--- AI/tree/node.rb
+++ AI/tree/node.rb
@@ -24,13 +24,30 @@ class Node
                 )
         end
         
-        def calculate_heuristic_value()
-                #if i'm a leaf
-                if son == nil
+        def calculate_heuristic_value(@board, current_player) #calculates personal heuristic_value and returns best value to father
+                var @brother_best_value, @best_value
+                #if i'm a leaf and i have a brother
+                if son == nil && if brother != nil
                         #i can calculate my value
                         @heuristic_value = heuristic.calculate_value(@board, current_player)
-                #if i'm a branch
-                else
-                        #i must ask my sons what their best value is
+                        #and then must choose the best value for my father
+                        @brother_best_value = @brother.calculate_heuristic_value(@board, current_player)
+                        if (current_player == A) #if i'm a min node
+                                #then the best value is the smallest value
+                                @brother_best_value < @heuristic_value ? @best_value = @brother_best_value : @best_value = @heuristic_value
+                        else #if i'm a max node
+                                #then the best value is the smallest value
+                                @brother_best_value > @heuristic_value ? @best_value = @brother_best_value : @best_value = @heuristic_value
+                        end
+                else if son == nil #&& if brother == nil
+                        #i can calculate my value
                         
+                        #and the best value is my value
+                else if son != nil && if brother != nil
+                        
+                else #if son != nil #&& if brother == nil
+                        
+                end
+                return @best_value
+        end
 end
