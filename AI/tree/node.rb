c23ef5315432fb1c74e390f48badef971e2751b7
JavaProcessingOctopus <frozenthunder@hotmail.com>
2015-10-08 16:45:14 +0200

    Changing logic of nodes 0.3

--- AI/tree/node.rb
+++ AI/tree/node.rb
@@ -27,11 +27,20 @@ class Node
         def calculate_heuristic_value(@board, current_player) #calculates personal heuristic_value and returns best value to father
                 var @brother_best_value, @best_value
                 #if i'm a leaf and i have a brother
-                if son == nil && if brother != nil
-                        #i can calculate my value
-                        @heuristic_value = heuristic.calculate_value(@board, current_player)
-                        #and then must choose the best value for my father
-                        @brother_best_value = @brother.calculate_heuristic_value(@board, current_player)
+                if son == nil
+						#i can calculate my value
+						@heuristic_value = heuristic.calculate_value(@board, @current_player)
+				#if i'm a branch
+				else
+						#i must ask my value to my son
+						@heuristic_value = @son.calculate_heuristic_value()
+				end
+                if brother == nil
+                        #the best value is my value
+                        @best_value = @heuristic_value
+                else
+						#i must choose the best value for my father
+						@brother_best_value = @brother.calculate_heuristic_value(@board, @current_player)
                         if (current_player == A) #if i'm a min node
                                 #then the best value is the smallest value
                                 @brother_best_value < @heuristic_value ? @best_value = @brother_best_value : @best_value = @heuristic_value
@@ -39,14 +48,6 @@ class Node
                                 #then the best value is the smallest value
                                 @brother_best_value > @heuristic_value ? @best_value = @brother_best_value : @best_value = @heuristic_value
                         end
-                else if son == nil #&& if brother == nil
-                        #i can calculate my value
-                        
-                        #and the best value is my value
-                else if son != nil && if brother != nil
-                        
-                else #if son != nil #&& if brother == nil
-                        
                 end
                 return @best_value
         end
