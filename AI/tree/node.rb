ee3647613434862655a5e100c91299ccaad34814
JavaProcessingOctopus <javaprocessingoctopus@outlook.com>
2015-10-09 09:17:47 +0200

    Syntax fixing
    If the syntax is correct now the next step would be to update algo.rb to
    make full use of the nodes.
    We might want to rename the node.rb file too. At it's core it has a
    MinMax logic.
    Maybe we can create an abstract node class from wich the specific nodes
    inherit.

--- AI/tree/node.rb
+++ AI/tree/node.rb
@@ -12,8 +12,8 @@ class Node
                        current_player, 
                        piece_played = nil
                       )
-                @son = son
-                @brother = brother
+                @son = son #maybe we could put a recursive call with a parameter for depth to build the tree from node.rb and not algo.rb
+                @brother = brother #same thing for brother
                 @board = board
                 @current_player = current_player
                 @heuristic = heuristic
@@ -21,31 +21,34 @@ class Node
                 @heuristic_value = nil
         end
         
-        def calculate_heuristic_value(@board, current_player) #calculates personal heuristic_value and returns best value to father
-                var @brother_best_value, @best_value
+        def calculate_heuristic_value() #calculates personal heuristic_value and returns best value to father
+                _best_value, _brother_best_value = 0, 0 #declaring and initializing the variable to be shure exist outside if statements
                 #if i'm a leaf and i have a brother
-                if son == nil
-						#i can calculate my value
-						@heuristic_value = heuristic.calculate_value(@board, @current_player)
-				#if i'm a branch
-				else
-						#i must ask my value to my son
-						@heuristic_value = @son.calculate_heuristic_value()
-				end
+                if @son == nil
+						            #i can calculate my value
+						            @heuristic_value = heuristic.calculate_value(@board, @current_player)
+				        #if i'm a branch
+				        else
+				                #i must ask my value to my son
+						            @heuristic_value = @son.calculate_heuristic_value()
+				        end
+				        
                 if brother == nil
                         #the best value is my value
-                        @best_value = @heuristic_value
+                        _best_value = @heuristic_value
                 else
-						#i must choose the best value for my father
-						@brother_best_value = @brother.calculate_heuristic_value(@board, @current_player)
-                        if (current_player == A) #if i'm a min node
+						    #i must choose the best value for my father
+						    _brother_best_value = @brother.calculate_heuristic_value()
+                        if (@current_player == 'A') #if i'm a min node
                                 #then the best value is the smallest value
-                                @brother_best_value < @heuristic_value ? @best_value = @brother_best_value : @best_value = @heuristic_value
-                        else #if i'm a max node
+                                _best_value = _brother_best_value < @heuristic_value ? _brother_best_value : @heuristic_value
+                        elsif (@current_player == 'B') #if i'm a max node
                                 #then the best value is the smallest value
-                                @brother_best_value > @heuristic_value ? @best_value = @brother_best_value : @best_value = @heuristic_value
+                                _best_value = _brother_best_value > @heuristic_value ? _brother_best_value : @heuristic_value
+                        else #this should not happpen
+                                #TODO write code for error case
                         end
                 end
-                return @best_value
+                return _best_value
         end
 end
