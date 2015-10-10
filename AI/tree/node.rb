9ec4c6a0239293c15e8ede3aa4f0d5565ce3e178
JavaProcessingOctopus <javaprocessingoctopus@outlook.com>
2015-10-10 11:22:21 +0200

    Working Version
    Code has been cleaned up and works with hill_climbing algorithm.
    Indentations should look better now too.

--- AI/tree/node.rb
+++ AI/tree/node.rb
@@ -19,36 +19,39 @@ class Node
                 @heuristic = heuristic
                 @piece_played = piece_played
                 @heuristic_value = nil
+                #puts "new node"
         end
         
         def calculate_heuristic_value() #calculates personal heuristic_value and returns best value to father
-                _best_value, _brother_best_value = 0, 0 #declaring and initializing the variable to be shure exist outside if statements
-                #if i'm a leaf and i have a brother
-                if @son == nil
-						            #i can calculate my value
-						            @heuristic_value = heuristic.calculate_value(@board, @current_player)
-				        #if i'm a branch
-				        else
-				                #i must ask my value to my son
-						            @heuristic_value = @son.calculate_heuristic_value()
-				        end
+                best_value, brother_best_value = nil, nil #declaring and initializing the variable to be shure exist outside if statements
+                
+                #searching for personal heuristic_value
+                if (@son == nil) #if i'm a leaf
+						#i can calculate my value
+						@heuristic_value = heuristic.calculate_value(@board, @current_player)
+				else #if i'm a branch
+						#i must ask my value to my son
+						@heuristic_value = @son.calculate_heuristic_value()
+				end
 				        
-                if brother == nil
-                        #the best value is my value
-                        _best_value = @heuristic_value
+				#searching for best_value to return to father
+                if (@brother == nil)
+						#the best value is my value
+						best_value = @heuristic_value
                 else
-						    #i must choose the best value for my father
-						    _brother_best_value = @brother.calculate_heuristic_value()
-                        if (@current_player == 'A') #if i'm a min node
+						#i must choose the best value for my father
+						brother_best_value = @brother.calculate_heuristic_value()
+						if (@current_player == 'A') #if i'm a min node(enemy turn) #TODO if values of current_player change, then correct this
+								#then the best value is the smallest value
+                                best_value = brother_best_value < @heuristic_value ? brother_best_value : @heuristic_value
+                        elsif (@current_player == 'B') #if i'm a max node(my turn) #TODO if values of current_player change, then correct this
                                 #then the best value is the smallest value
-                                _best_value = _brother_best_value < @heuristic_value ? _brother_best_value : @heuristic_value
-                        elsif (@current_player == 'B') #if i'm a max node
-                                #then the best value is the smallest value
-                                _best_value = _brother_best_value > @heuristic_value ? _brother_best_value : @heuristic_value
+                                best_value = brother_best_value > @heuristic_value ? brother_best_value : @heuristic_value
                         else #this should not happpen
                                 #TODO write code for error case
                         end
                 end
-                return _best_value
+                #returning best_value
+                return best_value
         end
 end
