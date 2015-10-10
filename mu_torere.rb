9649b44334462f30f4f6d8f015c8589249920137
JavaProcessingOctopus <javaprocessingoctopus@outlook.com>
2015-10-10 17:12:51 +0200

    Creating Alpha Beta Algorithm
    This version is runnable. Still have to do some checks to see if it runs
    the exact way i want it to.
    The various calls to calculate_heuristic_value() might make it a bit
    power hungry.
    Considering short-circuiting it when the value is already calculated.

--- mu_torere.rb
+++ mu_torere.rb
@@ -22,7 +22,7 @@ class Mu_Torere
                 @lost = false
                 @current_player = nil
                 @ai = AI.new(
-                        MinMax,
+                        Alpha_Beta,
                         Minimize_Ennemy_Plays,
                         'A'
                 )
