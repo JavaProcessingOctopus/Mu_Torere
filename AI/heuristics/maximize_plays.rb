2048fc86f0c419d8fee5c0de9c9f10e8f2add338
Rambo <mael.arnaud@gmail.com>
2015-10-11 17:18:19 +0200

    removed marks

--- AI/heuristics/maximize_plays.rb
+++ AI/heuristics/maximize_plays.rb
@@ -5,8 +5,6 @@ require_relative "../../board"
 class Maximize_Plays
         def self.calculate_value(board, player)
                 (1..9).inject(0) do |total, spot|
-                        puts "player : #{player}, spot : #{spot}"
-                        puts board.can_be_moved(spot, player)
                         total += board.can_be_moved(spot, player) ? 1 : 0
                 end
         end
